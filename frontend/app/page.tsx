e72c84641212166e3c925e8f8daf5caa1885e341
albonwu <albonwu@umich.edu>
2025-02-01 14:07:47 -0500

    make homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,34 +6,23 @@ import { button as buttonStyles } from "@heroui/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
+import {Input} from "@heroui/input";
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Make&nbsp;</span>
-        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
+        <span className={title({ color: "blue" })}>Supercharge&nbsp;</span>
+        <span className={title()}>your&nbsp;</span>
         <br />
-        <span className={title()}>
-          websites regardless of your design experience.
-        </span>
+        <span className={title()}>repository browsing.&nbsp;</span>
         <div className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
+        Enter the link to a GitHub repo below:
         </div>
       </div>
 
+      {/*
       <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
         <Link
           isExternal
           className={buttonStyles({ variant: "bordered", radius: "full" })}
@@ -43,11 +32,15 @@ export default function Home() {
           GitHub
         </Link>
       </div>
+*/}
+      <div className="flex gap-1 w-96">
+      <Input label="GitHub URL" type="url"/>
+      </div>
 
       <div className="mt-8">
         <Snippet hideCopyButton hideSymbol variant="bordered">
           <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
+            Or get started by replacing <Code color="primary">github.com</Code> in your repository's URL with <Code color="secondary">wayne.com</Code>
           </span>
         </Snippet>
       </div>
